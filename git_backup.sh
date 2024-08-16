e2d9e65d7459fc8935b9b5bebea5313a7493ce8e
DejaVuyan <46068933+DejaVuyan@users.noreply.github.com>
2024-08-16 14:27:44 +0800

    Create git_backup.sh
    增加了部分脚本文件

---
+++ git_backup.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+# 远程git仓库URL
+remote_git_repos=("git@github.com:DejaVuyan/ft_homework.git")
+
+# 本地git仓库根路径
+git_path="/home3/yangyuzun/homework"
+
+# 本地备份根目录
+backup_dir="/home3/yangyuzun/homework/git_backup"
+
+# 记录当前时间
+CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S")
